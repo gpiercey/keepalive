34ed6e37242a27788072e4d4b3c6ac22cd2a4536
Gary Piercey <gpiercey@users.noreply.github.com>
2021-01-05 20:32:55 -0330

    optimization

--- src/main/java/com/piercey/keepalive/Application.java
+++ src/main/java/com/piercey/keepalive/Application.java
@@ -5,34 +5,27 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Application {
-    public static void main(String[] args) {
-        final Point lastLocation = MouseInfo.getPointerInfo().getLocation();
+    public static void main(String[] args) throws Exception {
+        final Point previousLocation = MouseInfo.getPointerInfo().getLocation();
+        final Point currentLocation = MouseInfo.getPointerInfo().getLocation();
+        final Robot robot = new Robot();
+        final long delay = 120000L;
 
         final TimerTask wobbleMouse = new TimerTask() {
             public void run() {
-                try {
-                    final Point location = MouseInfo.getPointerInfo().getLocation();
-
-                    if (location.equals(lastLocation)) { // implies the system may be idle... so wobble the mouse cursor...
-                        final int x = (int) location.getX();
-                        final int y = (int) location.getY();
-
-                        final Robot robot = new Robot();
-                        robot.mouseMove(x + 1, y);
-                        robot.mouseMove(x, y);
-
-                    } else {
-                        lastLocation.setLocation(location);
-                    }
-
-                } catch (AWTException e) {
-                    e.printStackTrace();
-                    System.exit(1);
+                currentLocation.setLocation(MouseInfo.getPointerInfo().getLocation());
+                if (previousLocation.equals(currentLocation)) {
+                    final int x = (int) currentLocation.getX();
+                    final int y = (int) currentLocation.getY();
+                    robot.mouseMove(x + 1, y);
+                    robot.mouseMove(x, y);
+                } else {
+                    previousLocation.setLocation(currentLocation);
                 }
             }
         };
 
-        final Timer timer = new Timer("keepalive", false);
-        timer.schedule(wobbleMouse, 1000L, 1000L);
+        final Timer timer = new Timer("wobbler", false);
+        timer.schedule(wobbleMouse, delay, delay);
     }
 }
